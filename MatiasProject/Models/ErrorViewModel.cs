6f8e15e4807018a28692ef2f71092278a45de1e0
Mateusz Czernik <Mateusz.Czernik@microsoft.wsei.edu.pl>
2023-01-24 20:32:35 +0100

    Dodaj pliki projektów.

---
+++ MatiasProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MatiasProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
