c59fae676603ed22568a0cf42c4668debaa50827
Mateusz Czernik <Mateusz.Czernik@microsoft.wsei.edu.pl>
2023-01-25 18:47:09 +0100

    metody dla autora, dodanie wydawnictwa, poprawki w nazwach, zmiana linków

---
+++ MatiasProject/Repositories/Abstract/IWydawnictwoService.cs
@@ -0,0 +1,13 @@
+﻿using MatiasProject.Models.Domain;
+
+namespace MatiasProject.Repositories.Abstract
+{
+    public interface IWydawnictwoService
+    {
+        bool Add(Wydawnictwo model);
+        bool Update(Wydawnictwo model);
+        bool Delete(int model);
+        Wydawnictwo FindById(int id);
+        IEnumerable<Wydawnictwo> GetAll();
+    }
+}
---
+++ MatiasProject/Repositories/Implementation/WydawnictwoService.cs
@@ -0,0 +1,75 @@
+﻿using MatiasProject.Models.Domain;
+using MatiasProject.Repositories.Abstract;
+
+namespace MatiasProject.Repositories.Implementation
+{
+    public class WydawnictwoService : IWydawnictwoService
+    {
+        private readonly DatabaseContext context;
+
+        public WydawnictwoService(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        public bool Add(Wydawnictwo model)
+        {
+            try
+            {
+                context.Wydawnictwo.Add(model);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var data = this.FindById(id);
+                if (data == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    context.Wydawnictwo.Remove(data);
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public Wydawnictwo FindById(int id)
+        {
+            return context.Wydawnictwo.Find(id);
+        }
+
+        public IEnumerable<Wydawnictwo> GetAll()
+        {
+            return context.Wydawnictwo.ToList();
+        }
+
+        public bool Update(Wydawnictwo model)
+        {
+            try
+            {
+                context.Wydawnictwo.Update(model);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
