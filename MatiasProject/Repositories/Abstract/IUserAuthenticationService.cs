efc8bedf8c7f28445f2df2b8a6cdf7a46e73ea8c
Mateusz Czernik <Mateusz.Czernik@microsoft.wsei.edu.pl>
2023-01-26 17:30:01 +0100

    Authentication service

--- MatiasProject/Repositories/Abstract/IUserAuthenticationService.cs
+++ MatiasProject/Repositories/Abstract/IUserAuthenticationService.cs
@@ -1,6 +1,11 @@
-﻿namespace MatiasProject.Repositories.Abstract
+﻿using MatiasProject.Models.DTO;
+
+namespace MatiasProject.Repositories.Abstract
 {
     public interface IUserAuthenticationService
     {
+        Task<Status> LoginAsync(LoginModel model);
+        Task<Status> RegistrationAsync(RegistrationModel model);
+        Task LogoutAsync();
     }
 }
--- MatiasProject/Repositories/Implementation/UserAuthenticationService.cs
+++ MatiasProject/Repositories/Implementation/UserAuthenticationService.cs
@@ -1,8 +1,116 @@
-﻿using MatiasProject.Repositories.Abstract;
+﻿using MatiasProject.Models.Domain;
+using MatiasProject.Models.DTO;
+using MatiasProject.Repositories.Abstract;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace MatiasProject.Repositories.Implementation
 {
-    public class UserAuthenticationService:IUserAuthenticationService
+    public class UserAuthenticationService : IUserAuthenticationService
     {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public UserAuthenticationService(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        public async Task<Status> LoginAsync(LoginModel model)
+        {
+            var status = new Status();
+            var user = await userManager.FindByNameAsync(model.UserName);
+            if (user == null)
+            {
+                status.StatusCode = 0;
+                status.Message = "Niepoprawna nazwa użytkownika";
+                return status;
+            }
+
+            if (!await userManager.CheckPasswordAsync(user, model.Password))
+            {
+                status.StatusCode = 0;
+                status.Message = "Niepoprawne hasło";
+                return status;
+            }
+
+            var signInResult = await signInManager.PasswordSignInAsync(user,model.Password,false, true);
+            if (!signInResult.Succeeded) 
+            {
+                //dodanie roli
+                var userRoles = await userManager.GetRolesAsync(user);
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,user.UserName)
+                };
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+                status.StatusCode = 1;
+                status.Message = "Zalogowano pomyślnie";
+                return status;
+            }else if(signInResult.IsLockedOut)
+            {
+                status.StatusCode=0;
+                status.Message = "Użytkownik zablokowany";
+                return status;
+            }
+            else
+            {
+                status.StatusCode = 0;
+                status.Message = "Błąd przy próbie logowania";
+                return status;
+            }    
+        }
+
+        public async Task LogoutAsync()
+        {
+            await signInManager.SignOutAsync();
+        }
+
+        public async Task<Status> RegistrationAsync(RegistrationModel model)
+        {
+            var status = new Status();
+            var userExists = await userManager.FindByNameAsync(model.UserName);
+            if (userExists != null)
+            {
+                status.StatusCode = 0;
+                status.Message = "Taki użytkownik już istnieje";
+                return status;
+            }
+            ApplicationUser user = new ApplicationUser
+            {
+                SecurityStamp = Guid.NewGuid().ToString(),
+                Name = model.UserName,
+                Email = model.Email,
+                UserName = model.UserName,
+                EmailConfirmed = true
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                status.StatusCode = 0;
+                status.Message = "Nie udało się utworzyć użytkownika";
+                return status;
+            }
+
+            //zarządzanie rolami
+            if (!await roleManager.RoleExistsAsync(model.Role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(model.Role));
+            }
+            if (await roleManager.RoleExistsAsync(model.Role))
+            {
+                await userManager.AddToRoleAsync(user, model.Role);
+            }
+            status.StatusCode = 1;
+            status.Message = "Użytkownik został zarejestrowany pomyślnie";
+            return status;
+        }
     }
 }
