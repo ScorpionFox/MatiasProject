7ce70e43d280daec44112c78ce06a59bc8606561
Mateusz Czernik <Mateusz.Czernik@microsoft.wsei.edu.pl>
2023-01-26 16:44:21 +0100

    dodawanie authentication

--- MatiasProject/Models/Domain/DatabaseContext.cs
+++ MatiasProject/Models/Domain/DatabaseContext.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace MatiasProject.Models.Domain
 {
-    public class DatabaseContext:DbContext
+    public class DatabaseContext:IdentityDbContext<ApplicationUser>
     {
         public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options)
         {
--- MatiasProject/Program.cs
+++ MatiasProject/Program.cs
@@ -2,11 +2,14 @@ using Microsoft.EntityFrameworkCore;
 using MatiasProject.Models.Domain;
 using MatiasProject.Repositories.Abstract;
 using MatiasProject.Repositories.Implementation;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DatabaseContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("connstr")));
 
 builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("connstr")));
@@ -16,7 +19,11 @@ builder.Services.AddScoped<IGatunekService, GatunekService>();
 builder.Services.AddScoped<IAutorService, AutorService>();
 builder.Services.AddScoped<IWydawnictwoService, WydawnictwoService>();
 builder.Services.AddScoped<IBookService, BookService>();
-
+//Dla autoryzacji
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<DatabaseContext>()
+    .AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(op => op.LoginPath = "/UserAuthentication/Login");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,7 +38,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
